00066a2fd4b903e936549e1b9ba5576d73ad586c
Rafi udin <rafiudinonline@gmail.com>
2026-07-01 14:52:45 +0500

    Add Apartment Entity following domain driven design princeples

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,21 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    internal static readonly Currency None = new Currency("None");
+    public static readonly Currency USD = new Currency("USD");
+    public static readonly Currency EUR = new Currency("EUR");
+
+    private Currency(string code) => Code = code;
+
+    private string Code { get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return AllCurrencies.FirstOrDefault(c => c.Code == code) ??
+            throw new ArgumentException($"Invalid currency code: {code}");
+       
+    }
+
+    public static readonly IReadOnlyCollection<Currency> AllCurrencies = new List<Currency> { USD, EUR }.AsReadOnly();
+}
